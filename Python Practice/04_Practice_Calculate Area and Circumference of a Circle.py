bc7a161487f8c500cec2020a02c8946d31c275ce
Abhishek <abhimithun4u@gmail.com>
2024-02-17 01:42:28 +0530

    find area and circumference of a circle

---
+++ Python Practice/04_Practice_Calculate Area and Circumference of a Circle.py
@@ -0,0 +1,9 @@
+# Author: Abhishek Mukherjee
+# Program: Calculate Area and Circumference of a Circle
+pi = 3.14
+r = float(input("Enter the value of radius : \n"))
+print(r)
+area = float(pi*r*r)
+circumference = float(2*pi*r)
+print("The Area of the Circle with radius",r," is ",area)
+print("The Circumference of the Circle with radius",r," is ",circumference)
